3df90ffb84beb7a603705c4c8bb566a89abe1bd3
Gift <aghaulor.gift@gmail.com>
2024-08-04 18:51:45 -0700

    Add two numbers

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''A module that adds two numbers and return float type
+'''
+
+
+def add(a: float, b: float) -> float:
+    '''Adds two floating-point numbers.
+    '''
+    return a + b
