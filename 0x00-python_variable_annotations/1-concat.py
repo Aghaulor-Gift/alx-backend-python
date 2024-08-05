1789434f6b1ab0bbb2c222a74e2eac4ea084e29a
Gift <aghaulor.gift@gmail.com>
2024-08-04 18:56:01 -0700

    Use concatenation to add strings

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""This module creates a function that concatenates two strings"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    This concatenates two strings
+    """
+    return str1 + str2
