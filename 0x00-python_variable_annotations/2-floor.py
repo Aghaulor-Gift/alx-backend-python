95fcef8927605dd0351768baeb67cdda9a0f402c
Gift <aghaulor.gift@gmail.com>
2024-08-04 19:03:43 -0700

    Add function that uses floor values

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+This module contains a function that returns the rounded down integer of a
+float
+"""
+
+
+def floor(n: float) -> int:
+    """
+    It returns the rounded down integer of a float.
+    """
+    return int(n)
