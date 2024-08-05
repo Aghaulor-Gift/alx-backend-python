3e41d1ea4e7e1731b9b0a77a31edf507425ce10a
Gift <aghaulor.gift@gmail.com>
2024-08-04 19:09:37 -0700

    3-to_str.py

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""It returns the string representation of a float."""
+
+
+def to_str(n: float) -> str:
+    """
+    It returns the string representation of a float
+    """
+    return str(n)
